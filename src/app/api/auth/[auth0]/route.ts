96ff42b5b00fd94b92e2f2264686d3d05f31e423
ThaChozanJuan <benjinewman8@gmail.com>
2025-07-16 19:31:49 +1000

    Fix Auth0 import in route.ts

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -1,3 +1,6 @@
-import { handleAuth } from '@auth0/nextjs-auth0';
+import { initAuth0 } from '@auth0/nextjs-auth0';
 
-export const GET = handleAuth();
+export const GET = initAuth0().routeHandler;
+export const POST = initAuth0().routeHandler;
+
+export const runtime = 'edge';
