52fc2be3bcc83757cb59d1da5c3f740ec294ee63
ThaChozanJuan <benjinewman8@gmail.com>
2025-07-16 19:15:35 +1000

    Add route.ts

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -1,6 +1,3 @@
-import { initAuth0 } from '@auth0/nextjs-auth0/edge';
+import { handleAuth } from '@auth0/nextjs-auth0';
 
-export const GET = initAuth0().routeHandler;
-export const POST = initAuth0().routeHandler;
-
-export const runtime = 'edge';
+export const GET = handleAuth();
